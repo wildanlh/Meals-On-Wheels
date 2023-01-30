a67e22d90951a7f97ff289416ad8d755b90afd2f
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-01-30 14:38:14 +0700

    Add login page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import FormLogin from "../components/forms/FormLogin";
+import Layout from "../components/layout/Layout";
+
+const LoginPage = () => {
+    return (
+        <Layout>
+            <FormLogin />
+        </Layout>
+    );
+}
+
+export default LoginPage;
