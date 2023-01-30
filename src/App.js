9f2f9e3e9e80c5e54ce773d76bec13f9e0b3e1f6
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-01-30 10:31:52 +0700

    Add donate page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import DonatePage from './pages/DonatePage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
+      <Route path="/donate" element={<DonatePage />} />
     </Routes>
   );
 }
---
+++ src/pages/DonatePage.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout/Layout";
+
+import './css/DonatePage.css';
+
+const DonatePage = () => {
+    return (
+        <Layout>
+
+        </Layout>
+    );
+}
+
+export default DonatePage;
