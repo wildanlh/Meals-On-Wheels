50aed31abf5da3602ae16cd377c7d31dcc174e33
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-01 21:53:00 +0700

    Update home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { Button, Col, Row } from "react-bootstrap";
-import { banner1, banner2, botwave, logo, person, topwave } from "../assets";
+import { banner1, banner2, banner4, botwave, logo, person, topwave } from "../assets";
 import { Link } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 
@@ -15,7 +15,8 @@ const HomePage = () => {
                     </div>
                     <div className="col-lg-6 animate__animated animate__fadeInLeftBig" bis_skin_checked="1">
                         <h1 className="display-5 fw-bold lh-1 mb-3 ">Meals and Transportation Services. Together We Can Deliver</h1>
-                        <p>Lorem ipsum dolor sit amet ex letius nibh sit faucibus. A vivamus natoque auctor aliquet porta nibh taciti. Sapien euismod risus efficitur torquent consequat finibus. Habitant nibh platea porttitor vulputate amet pulvinar. </p>
+                        <div className="hr-purple"></div>
+                        <p>Our aspirations and guiding principles are determined by the community we serve, and we are driven by a strong commitment to our volunteers, donors, partners, and community.</p>
                     </div>
                 </div>
             </div>
@@ -25,9 +26,9 @@ const HomePage = () => {
                         <div className="col-lg-6" bis_skin_checked="1">
                             <h1 className="display-5 fw-bold lh-1 mb-3">Who We Are</h1>
                             <p>Lorem ipsum dolor sit amet ex letius nibh sit faucibus. A vivamus natoque auctor aliquet porta nibh taciti. Sapien euismod risus efficitur torquent consequat finibus. Habitant nibh platea porttitor vulputate amet pulvinar. </p>
-                            <h1 className="display-5 fw-bold lh-1 mb-3">Our Vision </h1>
+                            <h3 className="fw-bold lh-1 mb-3 purple-text">Our Vision </h3>
                             <p>Lorem ipsum dolor sit amet ex letius nibh sit faucibus</p>
-                            <h1 className="display-5 fw-bold lh-1 mb-3">Our Mission </h1>
+                            <h3 className="fw-bold lh-1 mb-3 purple-text">Our Mission </h3>
                             <p>Lorem ipsum dolor sit amet ex letius nibh sit faucibus</p>
                         </div>
                         <div className="col-10 col-sm-8 col-lg-6" bis_skin_checked="1">
@@ -41,7 +42,7 @@ const HomePage = () => {
                 <div className="container col-xxl-8 px-4 py-5" bis_skin_checked="1">
                     <div className="row flex-lg-row-reverse align-items-center g-5 py-5 animate__animated animate__fadeInLeftBig" bis_skin_checked="1">
                         <div className="col-10 col-sm-8 col-lg-6" bis_skin_checked="1">
-                            <img src={banner1} className="d-block mx-lg-auto img-fluid banner rounded" alt="Bootstrap Themes" loading="lazy" />
+                            <img src={banner4} className="d-block mx-lg-auto img-fluid banner rounded" alt="Bootstrap Themes" loading="lazy" />
                         </div>
                         <div className="col-lg-6" bis_skin_checked="1">
                             <h1 className="display-5 fw-bold lh-1 mb-3">Meals on Wheels Commitment</h1>
