fdceac95e5049672a945da5f5b406aad3e4049b9
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-02 15:25:18 +0700

    Update form register and partnership

--- src/components/forms/FormPartnership.js
+++ src/components/forms/FormPartnership.js
@@ -1,40 +1,53 @@
-import { FloatingLabel, Form, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { bannerportairt2 } from "../../assets";
+
+import './Form.css';
 
 const FormPartnership = () => {
     return (
-        <div className="container col-12 col-sm-3 col-lg-6 d-flex justify-content-center">
-            <div className="card m-5">
-                <Form className="p-3">
-                    <div className="text-center py-3">
-                        <h3 className="contact-title mx-3 text-white">Send a Partnership Request</h3>
-                        <hr className="text-white" />
+        <div className="container d-flex justify-content-center">
+            <div className="col-xxl-8 px-4 py-5">
+                <div className="card row flex-lg-row-reverse  py-5" bis_skin_checked="1">
+                    <h3 className="contact-title mx-3 text-white text-center">Partnership Request</h3>
+                    <hr className="text-white" />
+                    <div className="col-10 col-sm-8 col-lg-6 img-center" bis_skin_checked="1">
+                        <img src={bannerportairt2} className="rounded img-partner mb-3 pe-3" alt="Bootstrap Themes" loading="lazy" />
                     </div>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Company Name"
-                        className="mb-3 mx-3"
-                    >
-                        <Form.Control type="text" placeholder="Google" />
-                    </FloatingLabel>
-
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Email"
-                        className="mb-3 mx-3"
-                    >
-                        <Form.Control type="email" placeholder="email@email.com" />
-                    </FloatingLabel>
-                    <FloatingLabel controlId="floatingTextarea2" label="Reason" className="mb-3 mx-3">
-                        <Form.Control
-                            as="textarea"
-                            placeholder="Leave a comment here"
-                            style={{ height: '100px' }}
-                        />
-                    </FloatingLabel>
-                    <div className="text-center mb-2 d-grid mx-3 pt-3">
-                        <Button className="button fw-bold" size="lg">Send Request</Button>
+                    <div className="col-lg-6 text-center" bis_skin_checked="1">
+                        <Form className="px-3 text-white text-center">
+                            <Form.Group className="mb-3 mx-3" controlId="company-name">
+                                <Form.Label>Company Name</Form.Label>
+                                <Form.Control type="text" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="email">
+                                <Form.Label>Company Email</Form.Label>
+                                <Form.Control type="email" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="company-address">
+                                <Form.Label>Company Address</Form.Label>
+                                <Form.Control type="text" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="" required />
+                            </Form.Group>
+                            {/* <Form.Group className="mb-3 mx-3" controlId="file">
+                                <Form.Label>File Upload</Form.Label>
+                                <Form.Control type="file" placeholder="" required />
+                            </Form.Group> */}
+                            <div className="text-center mb-2 d-grid mx-3 pt-3">
+                                <Button type="submit" className="button fw-bold" size="lg">Send Request</Button>
+                                <div className="or-separator">
+                                    <span className="or-text">OR</span>
+                                </div>
+                                <Link to="/register" className="link-width w-100">
+                                    <Button className="button fw-bold w-100" size="lg">Register</Button>
+                                </Link>
+                            </div>
+                        </Form>
                     </div>
-                </Form>
+                </div>
             </div>
         </div>
     );
--- src/components/forms/FormRegister.js
+++ src/components/forms/FormRegister.js
@@ -1,7 +1,6 @@
-import { Button, Col, Form, Row } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { banner3 } from "../../assets";
-import TestFileUpload from "../../components/testFileUpload"
+import { bannerportairt1 } from "../../assets";
 
 import './Form.css';
 
@@ -9,9 +8,75 @@ const FormRegister = () => {
 
     return (
 
-        <div className="container col-12 col-sm-8 col-lg-6 d-flex justify-content-center">
-            <div className="card m-5 card-donate ">
-                <TestFileUpload/>
+        <div className="container d-flex justify-content-center">
+            <div className="col-xxl-8 px-4 py-5">
+                <div className="card row flex-lg-row-reverse  py-5" bis_skin_checked="1">
+                    <h3 className="contact-title mx-3 text-white text-center">REGISTER</h3>
+                    <hr className="text-white" />
+                    <div className="col-10 col-sm-8 col-lg-6 img-center" bis_skin_checked="1">
+                        <img src={bannerportairt1} className="rounded img-register mb-3 pe-3" alt="Bootstrap Themes" loading="lazy" />
+                    </div>
+                    <div className="col-lg-6 text-center" bis_skin_checked="1">
+                        <Form className="px-3 text-white text-center">
+                            <Form.Group className="mb-3 mx-3" controlId="fullname">
+                                <Form.Label>Full Name</Form.Label>
+                                <Form.Control type="text" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="address">
+                                <Form.Label>Address</Form.Label>
+                                <Form.Control type="text" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3 text" controlId="gender">
+                                <Form.Label>Gender</Form.Label>
+                                <Form.Select aria-label="Default select example" required>
+                                    <option selected="selected" disabled>Choose a gender</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="role">
+                                <Form.Label>Role</Form.Label>
+                                <Form.Select aria-label="Default select example" required>
+                                    <option selected="selected" disabled>Choose a role</option>
+                                    <option value="member">Member</option>
+                                    <option value="driver">Driver</option>
+                                    <option value="caregiver">Caregiver</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="" required />
+                            </Form.Group>
+                            <Form.Group className="mb-3 mx-3" controlId="file">
+                                <Form.Label>File Upload</Form.Label>
+                                <Form.Control type="file" placeholder="" required />
+                            </Form.Group>
+                            <div className="text-center mb-2 d-grid mx-3 pt-3">
+                                <Button type="submit" className="button fw-bold" size="lg">Register</Button>
+                                <div className="or-separator">
+                                    <span className="or-text">OR</span>
+                                </div>
+                                <Link to="/partnership" className="link-width w-100">
+                                    <Button className="button fw-bold w-100" size="lg">Partnership</Button>
+                                </Link>
+                            </div>
+
+                            <div className="text-center mt-3 p-3">
+                                <span>Already have an account?</span>
+                                <Link
+                                    to="/login"
+                                    className="ms-2 text-white"
+                                >
+                                    Login
+                                </Link>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
             </div>
         </div>
 
