1b1768bd2ba59066efd56ef8912e71cde84d039a
$learner <101691630+Bagus122-628@users.noreply.github.com>
2023-02-03 22:01:14 +0800

    login member work

--- src/pages/MemberHomePage.js
+++ src/pages/MemberHomePage.js
@@ -1,27 +1,27 @@
-import { useState } from "react";
-import { Carousel, Col, Container, Row, Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { carousel1, carousel2, carousel3 } from "../assets";
-import Layout from "../components/layout/Layout";
+import { useState } from "react"
+import { Carousel, Col, Container, Row, Table } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import { carousel1, carousel2, carousel3 } from "../assets"
+import Layout from "../components/layout/Layout"
 
-import "./css/MemberHomePage.css";
+import "./css/MemberHomePage.css"
 
 const MemberHomePage = () => {
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0)
 
   const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+    setIndex(selectedIndex)
+  }
 
   return (
     <Layout>
       <Container>
-        <Carousel activeIndex={index} onSelect={handleSelect} className="my-5">
+        <Carousel activeIndex={index} onSelect={handleSelect} className='my-5'>
           <Carousel.Item>
             <img
-              className="d-block w-100 carousel-member rounded"
+              className='d-block w-100 carousel-member rounded'
               src={carousel1}
-              alt="First slide"
+              alt='First slide'
             />
             <Carousel.Caption>
               <h3>Daily Meals</h3>
@@ -30,9 +30,9 @@ const MemberHomePage = () => {
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 carousel-member rounded"
+              className='d-block w-100 carousel-member rounded'
               src={carousel2}
-              alt="Second slide"
+              alt='Second slide'
             />
 
             <Carousel.Caption>
@@ -42,9 +42,9 @@ const MemberHomePage = () => {
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100 carousel-member rounded"
+              className='d-block w-100 carousel-member rounded'
               src={carousel3}
-              alt="Third slide"
+              alt='Third slide'
             />
 
             <Carousel.Caption>
@@ -55,34 +55,34 @@ const MemberHomePage = () => {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        <div className="meals-package mb-5">
-          <div className="col-lg-6 mb-3">
-            <h3 className="fw-bold">
+        <div className='meals-package mb-5'>
+          <div className='col-lg-6 mb-3'>
+            <h3 className='fw-bold'>
               Our Menus are in Packages to Help You Manage A Balanced Diet.
             </h3>
             <h5>Choose the package that is to your liking.</h5>
           </div>
-          <Row className="card-meals">
+          <Row className='card-meals'>
             <Col>
-              <Link to="/meals-package-detail">
-                <div className="card text-center">
-                  <h5 className="p-3">Meals Package Card</h5>
-                  <Table striped bordered className="fw-bold">
+              <Link to='/meals-package-detail'>
+                <div className='card text-center'>
+                  <h5 className='p-3'>Meals Package Card</h5>
+                  <Table striped bordered className='fw-bold'>
                     <tbody>
                       <tr>
-                        <td className="text-white">Roasted Duck</td>
+                        <td className='text-white'>Roasted Duck</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Greek Salad</td>
+                        <td className='text-white'>Greek Salad</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Miso Soup</td>
+                        <td className='text-white'>Miso Soup</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Fruit Tart</td>
+                        <td className='text-white'>Fruit Tart</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Teh Poci</td>
+                        <td className='text-white'>Teh Poci</td>
                       </tr>
                     </tbody>
                   </Table>
@@ -90,25 +90,25 @@ const MemberHomePage = () => {
               </Link>
             </Col>
             <Col>
-              <Link to="/meals-package-detail">
-                <div className="card text-center">
-                  <h5 className="p-3">Meals Package Card</h5>
-                  <Table striped bordered className="fw-bold">
+              <Link to='/meals-package-detail'>
+                <div className='card text-center'>
+                  <h5 className='p-3'>Meals Package Card</h5>
+                  <Table striped bordered className='fw-bold'>
                     <tbody>
                       <tr>
-                        <td className="text-white">Roasted Duck</td>
+                        <td className='text-white'>Roasted Duck</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Greek Salad</td>
+                        <td className='text-white'>Greek Salad</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Miso Soup</td>
+                        <td className='text-white'>Miso Soup</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Fruit Tart</td>
+                        <td className='text-white'>Fruit Tart</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Teh Poci</td>
+                        <td className='text-white'>Teh Poci</td>
                       </tr>
                     </tbody>
                   </Table>
@@ -116,25 +116,25 @@ const MemberHomePage = () => {
               </Link>
             </Col>
             <Col>
-              <Link to="/meals-package-detail">
-                <div className="card text-center">
-                  <h5 className="p-3">Meals Package Card</h5>
-                  <Table striped bordered className="fw-bold">
+              <Link to='/meals-package-detail'>
+                <div className='card text-center'>
+                  <h5 className='p-3'>Meals Package Card</h5>
+                  <Table striped bordered className='fw-bold'>
                     <tbody>
                       <tr>
-                        <td className="text-white">Roasted Duck</td>
+                        <td className='text-white'>Roasted Duck</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Greek Salad</td>
+                        <td className='text-white'>Greek Salad</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Miso Soup</td>
+                        <td className='text-white'>Miso Soup</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Fruit Tart</td>
+                        <td className='text-white'>Fruit Tart</td>
                       </tr>
                       <tr>
-                        <td className="text-white">Teh Poci</td>
+                        <td className='text-white'>Teh Poci</td>
                       </tr>
                     </tbody>
                   </Table>
@@ -145,7 +145,7 @@ const MemberHomePage = () => {
         </div>
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default MemberHomePage;
+export default MemberHomePage
