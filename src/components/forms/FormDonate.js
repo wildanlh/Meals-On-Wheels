ebebc01ee2aaf89e15b3538b8513aaac15b1a11a
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-03 14:22:30 +0700

    Update donate page

--- src/components/forms/FormDonate.js
+++ src/components/forms/FormDonate.js
@@ -1,8 +1,7 @@
 import { PaymentElement } from "@stripe/react-stripe-js";
 import { useState } from "react";
 import { useStripe, useElements } from "@stripe/react-stripe-js";
-import "../../pages/css/DonatePage.css"
-
+import "../../pages/css/DonatePage.css";
 
 export default function FormDonate() {
   const stripe = useStripe();
@@ -23,26 +22,40 @@ export default function FormDonate() {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-      return_url: `${window.location.origin}/ThankYou`,
-    },
+        return_url: `${window.location.origin}/ThankYou`,
+      },
     });
 
     if (error.type === "card_error" || error.type === "validation_error") {
       setMessage(error.message);
     } else {
-      setMessage("An unexpected error occured.")
+      setMessage("An unexpected error occured.");
     }
 
     setIsProcessing(false);
   };
 
   return (
-    <form id="donate-form" onSubmit={handleSubmit}>
-      <PaymentElement id="payment-element" />
-        <button disabled={isProcessing || !stripe || !elements} className="button fw-bold mt-2" id="submit">
-          <span id="button-text">{isProcessing ? "Processing. . ." : "Donate Now"}</span>
-        </button>
-      {message && <div id="payment-message">{message}</div>}
-    </form>
+    <div className="d-flex d-flex justify-content-center mb-5">
+      <div className="w-50">
+        <form
+          id="donate-form"
+          onSubmit={handleSubmit}
+          className="rounded border-1"
+        >
+          <PaymentElement id="payment-element" />
+          <button
+            disabled={isProcessing || !stripe || !elements}
+            className="button fw-bold mt-3 text-white rounded px-3"
+            id="submit"
+          >
+            <span id="button-text">
+              {isProcessing ? "Processing. . ." : "Donate Now"}
+            </span>
+          </button>
+          {message && <div id="payment-message">{message}</div>}
+        </form>
+      </div>
+    </div>
   );
 }
