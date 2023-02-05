83f34fe7abda0651faf54c41f2f71f76e6de682e
$learner <101691630+Bagus122-628@users.noreply.github.com>
2023-02-05 22:36:37 +0800

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,99 +1,99 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { useContext } from "react";
-import AuthContext from "./context/auth-context";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "animate.css";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import DonatePage from "./pages/DonatePage";
-import PartnershipPage from "./pages/PartnershipPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import TermsAndCondition from "./pages/TermsAndConditionPage";
-import MemberHomePage from "./pages/MemberHomePage";
-import MemberMealPackageDetailPage from "./pages/MemberMealPackageDetailPage";
-import MemberFeedbackPage from "./pages/MemberFeedbackPage";
-import DriverHomePage from "./pages/DriverHomePage";
-import CaregiverHomePage from "./pages/CaregiverHomePage";
-import AdminHomePage from "./pages/AdminHomePage";
-import AdminDonationHistoryPage from "./pages/AdminDonationHistoryPage";
-import AdminManagePartnershipPage from "./pages/AdminManagePartnershipPage";
-import AdminManageUsersPage from "./pages/AdminManageUsersPage";
-import TestRegister from "./components/forms/TestRegister";
-import ThankYouPage from "./pages/ThankYouPage";
-import MemberOrderHistoryPage from "./pages/MemberOrderHistoryPage";
-import PartnerHomePage from "./pages/PartnerHomePage";
+import { Navigate, Route, Routes } from "react-router-dom"
+import { useContext } from "react"
+import AuthContext from "./context/auth-context"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import "animate.css"
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+import DonatePage from "./pages/DonatePage"
+import PartnershipPage from "./pages/PartnershipPage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import TermsAndCondition from "./pages/TermsAndConditionPage"
+import MemberHomePage from "./pages/MemberHomePage"
+import MemberMealPackageDetailPage from "./pages/MemberMealPackageDetailPage"
+import MemberFeedbackPage from "./pages/MemberFeedbackPage"
+import DriverHomePage from "./pages/DriverHomePage"
+import CaregiverHomePage from "./pages/CaregiverHomePage"
+import AdminHomePage from "./pages/AdminHomePage"
+import AdminDonationHistoryPage from "./pages/AdminDonationHistoryPage"
+import AdminManagePartnershipPage from "./pages/AdminManagePartnershipPage"
+import AdminManageUsersPage from "./pages/AdminManageUsersPage"
+import TestRegister from "./components/forms/TestRegister"
+import ThankYouPage from "./pages/ThankYouPage"
+import MemberOrderHistoryPage from "./pages/MemberOrderHistoryPage"
+import PartnerHomePage from "./pages/PartnerHomePage"
 
 function App() {
-  const { isLoggedIn, currentUser } = useContext(AuthContext);
+  const { isLoggedIn, currentUser } = useContext(AuthContext)
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/donate" element={<DonatePage />} />
-      <Route path="/thankyou" element={<ThankYouPage />} />
-      <Route path="/Test" element={<TestRegister />} />
-      <Route path="/partnership" element={<PartnershipPage />} />
-      
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/contact' element={<ContactPage />} />
+      <Route path='/donate' element={<DonatePage />} />
+      <Route path='/thankyou' element={<ThankYouPage />} />
+      <Route path='/Test' element={<TestRegister />} />
+      <Route path='/partnership' element={<PartnershipPage />} />
+
       {!isLoggedIn && (
         <>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/terms" element={<TermsAndCondition />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/terms' element={<TermsAndCondition />} />
         </>
       )}
 
       {isLoggedIn && (
         <>
-        {/* Login as Member Access */}
+          {/* Login as Member Access */}
           {currentUser.role === "ROLE_MEMBER" && (
             <>
-              <Route path="/home" element={<MemberHomePage />} />
+              <Route path='/home' element={<MemberHomePage />} />
               <Route
-                path="/home/order-history"
+                path='/home/order-history'
                 element={<MemberOrderHistoryPage />}
               />
-              <Route path="/feedback" element={<MemberFeedbackPage />} />
+              <Route path='/feedback' element={<MemberFeedbackPage />} />
               <Route
-                path="/meals-package-detail/:menuId"
+                path='/meals-package-detail/:menuId'
                 element={<MemberMealPackageDetailPage />}
               />
             </>
           )}
 
-        {/* Login as Driver Access */}
-          {currentUser.role === "ROLE_DRIVER" && (
-            <Route path="/driver" element={<DriverHomePage />} />
+          {/* Login as Driver Access */}
+          {currentUser.role === "ROLE_RIDER" && (
+            <Route path='/driver' element={<DriverHomePage />} />
           )}
 
-        {/* Login as Caregiver Access */}
+          {/* Login as Caregiver Access */}
           {currentUser.role === "ROLE_CAREGIVER" && (
-            <Route path="/caregiver" element={<CaregiverHomePage />} />
+            <Route path='/caregiver' element={<CaregiverHomePage />} />
           )}
 
-        {/* Login as Partner Access */}
+          {/* Login as Partner Access */}
           {currentUser.role === "ROLE_PARTNER" && (
-            <Route path="/partner" element={<PartnerHomePage />} />
+            <Route path='/partner' element={<PartnerHomePage />} />
           )}
 
-        {/* Login as Admin Access */}
+          {/* Login as Admin Access */}
           {currentUser.role === "ROLE_ADMIN" && (
             <>
-              <Route path="/admin" element={<AdminHomePage />} />
+              <Route path='/admin' element={<AdminHomePage />} />
               <Route
-                path="/admin/donation-history"
+                path='/admin/donation-history'
                 element={<AdminDonationHistoryPage />}
               />
               <Route
-                path="/admin/manage-partner"
+                path='/admin/manage-partner'
                 element={<AdminManagePartnershipPage />}
               />
               <Route
-                path="/admin/manage-users"
+                path='/admin/manage-users'
                 element={<AdminManageUsersPage />}
               />
             </>
@@ -101,7 +101,7 @@ function App() {
         </>
       )}
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
