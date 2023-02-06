daf5ef50ffbd4b19756297db98f3c714acf54338
$learner <101691630+Bagus122-628@users.noreply.github.com>
2023-02-06 13:30:57 +0800

    done bang, ijin off dulu ya

--- src/pages/AdminManagePartnershipPage.js
+++ src/pages/AdminManagePartnershipPage.js
@@ -1,3 +1,4 @@
+import { Fragment, useContext, useEffect, useState } from "react"
 import {
   Button,
   Col,
@@ -7,67 +8,97 @@ import {
   Tab,
   Table,
   Tabs,
-} from "react-bootstrap";
-import { person } from "../assets";
-import Layout from "../components/layout/Layout";
+} from "react-bootstrap"
+import { person } from "../assets"
+import Layout from "../components/layout/Layout"
+import AuthContext from "../context/auth-context"
+import { getAdminPartnerAPI, getAdminPartnerActiveAPI } from "../api/admin-api"
 
-import "./css/AdminManagePartnershipPage.css";
+import "./css/AdminManagePartnershipPage.css"
+import { user_type } from "../context/context-type"
 
 const AdminManagePartnershipPage = () => {
+  const { token } = useContext(AuthContext)
+  const [partner, setPartner] = useState([user_type])
+  const [msg, setMsg] = useState("")
+  function handleAccept(id) {
+    getAdminPartnerActiveAPI(token, id)
+      .then((resp) => setMsg(resp.data.message))
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    getAdminPartnerAPI(token)
+      .then((resp) => setPartner(resp.data))
+      .catch((err) => console.table(err))
+  }, [token, msg])
+
   return (
     <Layout>
       <Container>
-        <h1 className="text-center py-5 fw-bold">Manage Partner</h1>
+        <h1 className='text-center py-5 fw-bold'>Manage Partner</h1>
         <Tabs
-          defaultActiveKey="profile"
-          id="fill-tab-example"
-          className="mb-3 fw-bold tab-partner"
+          defaultActiveKey='profile'
+          id='fill-tab-example'
+          className='mb-3 fw-bold tab-partner'
           fill
         >
-          <Tab eventKey="partnership-request" title="Partnership Request">
-            <Form.Control type="text" placeholder="Search" className="my-3" />
-            <div className="card-charity">
+          <Tab eventKey='partnership-request' title='Partnership Request'>
+            <Form.Control type='text' placeholder='Search' className='my-3' />
+            {msg}
+            <div className='card-charity'>
               <Row>
-                <Col sm={9}>
-                  <div className="d-flex">
-                    <img src={person} alt="" />
-                    <h3>Charity Company Ltd</h3>
-                  </div>
-                </Col>
-                <Col sm={3}>
-                  <div className="text-center p-3">
-                    <Button className="button fw-bold">Accept Partnership</Button>
-                  </div>
-                </Col>
+                {partner.map((data) => (
+                  <Fragment key={data.id}>
+                    <Col sm={9}>
+                      <div className='d-flex'>
+                        <img src={data.imageUrl} alt='' />
+                        <h3>{data.name}</h3>
+                      </div>
+                    </Col>
+                    <Col sm={3}>
+                      <div className='text-center p-3'>
+                        <Button
+                          className='button fw-bold'
+                          onClick={() => handleAccept(data.id)}
+                        >
+                          Accept Partnership
+                        </Button>
+                      </div>
+                    </Col>
+                  </Fragment>
+                ))}
               </Row>
             </div>
           </Tab>
-          <Tab eventKey="registered-partner" title="Registered Partner">
-            <div className="card-history my-5">
-              <h5 className="fw-bold mx-3">April 20, 2020</h5>
+          <Tab eventKey='registered-partner' title='Registered Partner'>
+            <div className='card-history my-5'>
+              <h5 className='fw-bold mx-3'>April 20, 2020</h5>
 
               <Table
                 striped
-                className="text-white text-center history-table mb-5"
+                className='text-white text-center history-table mb-5'
               >
-                <thead className="history-table text-dark">
+                <thead className='history-table text-dark'>
                   <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
+                    <th>Company Name</th>
+                    <th>Address</th>
                     <th>Amount</th>
                     <th>Message</th>
                   </tr>
                 </thead>
-                <tbody className="text-white">
-                  <tr>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>$10</td>
-                    <td>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Incidunt, ad?
-                    </td>
-                  </tr>
+                <tbody className='text-white'>
+                  {partner.map((data) => (
+                    <tr key={data.id}>
+                      <td>{data.name}</td>
+                      <td>{data.address}</td>
+                      <td>$10 lawak ini darimana?</td>
+                      <td>
+                        ini juga dari mana? Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Incidunt, ad?
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </Table>
             </div>
@@ -75,7 +106,7 @@ const AdminManagePartnershipPage = () => {
         </Tabs>
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default AdminManagePartnershipPage;
+export default AdminManagePartnershipPage
--- src/pages/AdminMealHistoryPage.js
+++ src/pages/AdminMealHistoryPage.js
@@ -1,47 +1,75 @@
-import { Container, Form, Table } from "react-bootstrap";
-import Layout from "../components/layout/Layout";
+import { useContext, useEffect, useState } from "react"
+import { Container, Form, Table } from "react-bootstrap"
+import { getAdminOrderAllAPI } from "../api/admin-api"
+import Layout from "../components/layout/Layout"
+import AuthContext from "../context/auth-context"
+import { order_type } from "../context/context-type"
 
-import "./css/AdminDonationHistoryPage.css";
+import "./css/AdminDonationHistoryPage.css"
 
 const AdminMealHistoryPage = () => {
+  const { token } = useContext(AuthContext)
+  const [order, setOrder] = useState([order_type])
+
+  console.log(order.sort((a, b) => b.id - a.id))
+
+  useEffect(() => {
+    getAdminOrderAllAPI(token)
+      .then((resp) => setOrder(resp.data))
+      .catch((err) => console.log(err))
+
+    return () => {}
+  }, [token])
+
   return (
     <Layout>
       <Container>
-        <h1 className="text-center py-5 fw-bold">Meal History</h1>
-        <Form.Select className="fw-bold mb-5">
+        <h1 className='text-center py-5 fw-bold'>Meal History</h1>
+        <Form.Select className='fw-bold mb-5'>
           <option>2022</option>
-          <option>2021</option>
+          <option>gimana cara implemen ini?</option>
           <option>2020</option>
           <option>2019</option>
         </Form.Select>
 
-        <div className="card-history">
-          <h5 className="fw-bold mx-3">April 20, 2020 (Display Date)</h5>
+        <div className='card-history'>
+          <h5 className='fw-bold mx-3'>April 20, 2020 (Display Date)</h5>
 
-          <Table striped className="text-white text-center history-table mb-5">
-            <thead className="history-table text-dark">
+          <Table striped className='text-white text-center history-table mb-5'>
+            <thead className='history-table text-dark'>
               <tr>
+                <th>No</th>
                 <th>Name</th>
                 <th>Package Name</th>
+                <th>Deliver to</th>
                 <th>Order On</th>
+                <th>Prepare by</th>
+                <th>Deliver by</th>
                 <th>Status</th>
-                <th>Tambah aja kalau ada yang kurang</th>
               </tr>
             </thead>
-            <tbody className="text-white">
-              <tr>
-                <td>John</td>
-                <td>Meal 1</td>
-                <td>Jan 23, 2023</td>
-                <td>Completed</td>
-                <td>Tamabh aja klau ada yang kurang</td>
-              </tr>
+            <tbody className='text-white'>
+              {order
+                .sort((a, b) => b.id - a.id)
+                .map((x, i) => (
+                  <tr key={x.id}>
+                    <td>{i + 1}</td>
+                    <td>{x.orderBy.name}</td>
+                    <td>{x.orderBy.name}</td>
+                    <td>{x.mealPackage.packageName}</td>
+                    <td>{x.orderBy.address}</td>
+                    <td>{Date(x.orderOn).slice(0, 10)}</td>
+                    <td>{x.preparedBy?.name}</td>
+                    <td>{x.deliveredBy?.name}</td>
+                    <td>{x.orderStatus}</td>
+                  </tr>
+                ))}
             </tbody>
           </Table>
         </div>
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default AdminMealHistoryPage;
+export default AdminMealHistoryPage
