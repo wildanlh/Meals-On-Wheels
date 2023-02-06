01090c0653d61300db7653d20a8bb38b6ef86283
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-06 15:08:53 +0700

    Update caregiver home page

--- src/pages/CaregiverHomePage.js
+++ src/pages/CaregiverHomePage.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react";
 import {
   Button,
   Carousel,
@@ -8,12 +8,13 @@ import {
   Modal,
   Row,
   Table,
-} from "react-bootstrap"
+} from "react-bootstrap";
 import {
   getPartnerOrderAPI,
   postPartnerOrderCompleteAPI,
   postPartnerOrderCreateAPI,
-} from "../api/partner-api"
+} from "../api/partner-api";
+import { getMenu } from "../api/api";
 import {
   carousel1,
   carousel2,
@@ -21,58 +22,67 @@ import {
   greencircle,
   redcircle,
   yellowcircle,
-} from "../assets"
-import Layout from "../components/layout/Layout"
-import AuthContext from "../context/auth-context"
-import { order_type } from "../context/context-type"
+} from "../assets";
+import Layout from "../components/layout/Layout";
+import AuthContext from "../context/auth-context";
+import { order_type, menu_type } from "../context/context-type";
 
-import "./css/CaregiverHomePage.css"
+import "./css/CaregiverHomePage.css";
 
 const CaregiverHomePage = () => {
-  const { token, currentUser } = useContext(AuthContext)
-  const [msg, setMsg] = useState("")
-  const [orderList, setOrderList] = useState([order_type])
-  const [index, setIndex] = useState(0)
+  const { token, currentUser } = useContext(AuthContext);
+  const [msg, setMsg] = useState("");
+  const [orderList, setOrderList] = useState([order_type]);
+  const [index, setIndex] = useState(0);
+  const [menu, setMenu] = useState([menu_type]);
 
   const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex)
-  }
+    setIndex(selectedIndex);
+  };
 
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   function handlePrepare(id) {
     postPartnerOrderCreateAPI(token, id)
       .then((resp) => setMsg(resp.data.message))
-      .catch((err) => console.log(err.response))
+      .catch((err) => console.log(err.response));
   }
 
   function handleComplate(id) {
     postPartnerOrderCompleteAPI(token, id)
       .then((resp) => setMsg(resp.data.message))
-      .catch((err) => console.log(err.response))
+      .catch((err) => console.log(err.response));
   }
 
   useEffect(() => {
+    getMenu(token)
+      .then((resp) => {
+        setMenu(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     getPartnerOrderAPI(token)
       .then((resp) => setOrderList(resp.data))
-      .catch((err) => console.log(err.response))
-  }, [token, msg])
+      .catch((err) => console.log(err.response));
+  }, [token, msg]);
   return (
     <Layout>
       <Container>
-        <h1 className='py-5 fw-bold'>Hello, John Doe!</h1>
+        <h1 className="py-5 fw-bold">Hello, John Doe!</h1>
 
-        <Row className='mb-5'>
+        <Row className="mb-5">
           <Col sm={8}>
             <Carousel activeIndex={index} onSelect={handleSelect}>
               <Carousel.Item>
                 <img
-                  className='d-block w-100 carousel-member rounded'
+                  className="d-block w-100 carousel-member rounded"
                   src={carousel1}
-                  alt='First slide'
+                  alt="First slide"
                 />
                 <Carousel.Caption>
                   <h3>Daily Meals</h3>
@@ -83,9 +93,9 @@ const CaregiverHomePage = () => {
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className='d-block w-100 carousel-member rounded'
+                  className="d-block w-100 carousel-member rounded"
                   src={carousel2}
-                  alt='Second slide'
+                  alt="Second slide"
                 />
 
                 <Carousel.Caption>
@@ -97,9 +107,9 @@ const CaregiverHomePage = () => {
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className='d-block w-100 carousel-member rounded'
+                  className="d-block w-100 carousel-member rounded"
                   src={carousel3}
-                  alt='Third slide'
+                  alt="Third slide"
                 />
 
                 <Carousel.Caption>
@@ -113,34 +123,30 @@ const CaregiverHomePage = () => {
             </Carousel>
           </Col>
           <Col sm={4}>
-            <h4 className='text-center fw-bold title-caregiver'>
+            <h4 className="text-center fw-bold title-caregiver">
               Meal Package List
             </h4>
-            <div className='card'>
-              <Table striped className='text-white text-center driver mb-3'>
-                <thead className='driver-table'>
+            <div className="card">
+              <Table striped className="text-white text-center driver mb-3">
+                <thead className="driver-table">
                   <tr>
                     <th>Meal</th>
                   </tr>
                 </thead>
-                <tbody className='text-white'>
-                  <tr>
-                    <td className='text-white'>1</td>
-                  </tr>
-                  <tr>
-                    <td className='text-white'>1</td>
-                  </tr>
-                  <tr>
-                    <td className='text-white'>1</td>
-                  </tr>
-                </tbody>
+                {menu.slice(0, 3).map((data) => (
+                  <tbody className="text-white" key={data.id}>
+                    <tr>
+                      <td className="text-white">{data.packageName}</td>
+                    </tr>
+                  </tbody>
+                ))}
               </Table>
 
-              <div className='text-center fw-bold py-3'>
+              <div className="text-center fw-bold">
                 <Button
-                  variant='primary'
+                  variant="primary"
                   onClick={handleShow}
-                  className='button'
+                  className="button my-3"
                 >
                   + Add Meal Package
                 </Button>
@@ -149,13 +155,13 @@ const CaregiverHomePage = () => {
           </Col>
         </Row>
 
-        <Row className='mb-5'>
+        <Row className="mb-5">
           <Col sm={8}>
-            <h4 className='fw-bold title-caregiver'>Order Notification</h4>
-            <div className='card'>
-              <div className='container'>
-                <Table striped className='text-white text-center driver my-3'>
-                  <thead className='driver-table'>
+            <h4 className="fw-bold title-caregiver">Order Notification</h4>
+            <div className="card">
+              <div className="container">
+                <Table striped className="text-white text-center driver my-3">
+                  <thead className="driver-table">
                     <tr>
                       <th>No</th>
                       <th>Request</th>
@@ -163,16 +169,16 @@ const CaregiverHomePage = () => {
                       <th>Time</th>
                     </tr>
                   </thead>
-                  <tbody className='text-white'>
+                  <tbody className="text-white">
                     {orderList.map((order, index) => (
                       <tr key={order.id}>
-                        <td className='text-white'>{index + 1}</td>
-                        <td className='text-white'>
+                        <td className="text-white">{index + 1}</td>
+                        <td className="text-white">
                           {order.orderBy.name} requested for{" "}
                           {order.mealPackage.packageName}
                         </td>
-                        <td className='text-white'>{order.orderOn}</td>
-                        <td className='text-white'>5:00 PM</td>
+                        <td className="text-white">{order.orderOn}</td>
+                        <td className="text-white">5:00 PM</td>
                       </tr>
                     ))}
                     {/* <tr>
@@ -197,38 +203,38 @@ const CaregiverHomePage = () => {
             </div>
           </Col>
           <Col sm={4}>
-            <h4 className='text-center fw-bold title-caregiver'>
+            <h4 className="text-center fw-bold title-caregiver">
               Driver Availability
             </h4>
-            <div className='card'>
-              <Table striped className='text-white text-center driver mb-3'>
-                <thead className='driver-table'>
+            <div className="card">
+              <Table striped className="text-white text-center driver mb-3">
+                <thead className="driver-table">
                   <tr>
                     <th>Name</th>
                   </tr>
                 </thead>
-                <tbody className='text-white'>
+                <tbody className="text-white">
                   <tr>
-                    <td className='text-white'>1</td>
+                    <td className="text-white">1</td>
                   </tr>
                   <tr>
-                    <td className='text-white'>1</td>
+                    <td className="text-white">1</td>
                   </tr>
                   <tr>
-                    <td className='text-white'>1</td>
+                    <td className="text-white">1</td>
                   </tr>
                 </tbody>
               </Table>
             </div>
           </Col>
         </Row>
-        <div className='task pb-5'>
-          <h4 className='fw-bold title-caregiver'>Task</h4>
+        <div className="task pb-5">
+          <h4 className="fw-bold title-caregiver">Task</h4>
           {msg && <Button onClick={() => setMsg("")}>{msg}</Button>}
-          <div className='card'>
-            <div className='container'>
-              <Table striped className='text-white text-center driver my-3'>
-                <thead className='driver-table'>
+          <div className="card">
+            <div className="container">
+              <Table striped className="text-white text-center driver my-3">
+                <thead className="driver-table">
                   <tr>
                     <th>No</th>
                     <th>Meals Request List</th>
@@ -237,23 +243,23 @@ const CaregiverHomePage = () => {
                     <th>action</th>
                   </tr>
                 </thead>
-                <tbody className='text-white'>
+                <tbody className="text-white">
                   {orderList.map((order, index) => (
                     <tr key={order.id}>
-                      <td className='text-white'>{index + 1}</td>
-                      <td className='text-white'>
+                      <td className="text-white">{index + 1}</td>
+                      <td className="text-white">
                         {order.mealPackage.packageName}
                       </td>
-                      <td className='text-white'>
-                        <div className='status text-white d-flex justify-content-center'>
-                          <img src={redcircle} alt='' className='status-icon' />
-                          <span className='fw-bold ms-3'>
+                      <td className="text-white">
+                        <div className="status text-white d-flex justify-content-center">
+                          <img src={redcircle} alt="" className="status-icon" />
+                          <span className="fw-bold ms-3">
                             {order.orderStatus}
                           </span>
                         </div>
                       </td>
-                      <td className='text-white'>{order.orderOn}</td>
-                      <td className='text-white'>
+                      <td className="text-white">{order.orderOn}</td>
+                      <td className="text-white">
                         {order.orderStatus === "PENDING" ? (
                           <Button onClick={() => handlePrepare(order.id)}>
                             prepare
@@ -301,67 +307,67 @@ const CaregiverHomePage = () => {
         </div>
       </Container>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton className='modal-popup'>
-          <div className='text-center'>
-            <Modal.Title className='text-white fw-bold'>
+        <Modal.Header closeButton className="modal-popup">
+          <div className="text-center">
+            <Modal.Title className="text-white fw-bold">
               Add Package
             </Modal.Title>
           </div>
         </Modal.Header>
-        <Modal.Body className='modal-popup'>
+        <Modal.Body className="modal-popup">
           <Form>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label className='text-white fw-bold'>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label className="text-white fw-bold">
                 Main Course
               </Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Ex: Roasted Duck/ Spicy Thai Chicken/ect'
+                type="text"
+                placeholder="Ex: Roasted Duck/ Spicy Thai Chicken/ect"
                 autoFocus
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label className='text-white fw-bold'>Salad</Form.Label>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label className="text-white fw-bold">Salad</Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Ex: garden salad/Greek salad/chopped Thai salad'
+                type="text"
+                placeholder="Ex: garden salad/Greek salad/chopped Thai salad"
                 autoFocus
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label className='text-white fw-bold'>Soup</Form.Label>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label className="text-white fw-bold">Soup</Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Ex: Pumpkin soup/Tuscan/ect'
+                type="text"
+                placeholder="Ex: Pumpkin soup/Tuscan/ect"
                 autoFocus
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label className='text-white fw-bold'>Dessert</Form.Label>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label className="text-white fw-bold">Dessert</Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Ex: Pudding/ fruit tarts/ lemon creme/ ect'
+                type="text"
+                placeholder="Ex: Pudding/ fruit tarts/ lemon creme/ ect"
                 autoFocus
               />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-              <Form.Label className='text-white fw-bold'>Drink</Form.Label>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label className="text-white fw-bold">Drink</Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Ex: Carrot Juice/ Liang Tea/ Teh Poci/ ect'
+                type="text"
+                placeholder="Ex: Carrot Juice/ Liang Tea/ Teh Poci/ ect"
                 autoFocus
               />
             </Form.Group>
           </Form>
         </Modal.Body>
-        <div className='text-center modal-popup p-3'>
-          <Button onClick={handleClose} className='button fw-bold w-50'>
+        <div className="text-center modal-popup p-3">
+          <Button onClick={handleClose} className="button fw-bold w-50">
             Submit
           </Button>
         </div>
       </Modal>
     </Layout>
-  )
-}
+  );
+};
 
-export default CaregiverHomePage
+export default CaregiverHomePage;
