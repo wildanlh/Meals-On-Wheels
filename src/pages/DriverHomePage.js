17b6d837de775252e6ba7fe1d16c08e07172f19c
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-07 06:16:29 +0700

    Update driver home page

--- src/pages/DriverHomePage.js
+++ src/pages/DriverHomePage.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react";
 import {
   Button,
   Carousel,
@@ -8,13 +8,13 @@ import {
   DropdownButton,
   Row,
   Table,
-} from "react-bootstrap"
+} from "react-bootstrap";
 import {
   getRiderOrderAPI,
   postRiderOrderCompleteAPI,
   postRiderOrderCreateAPI,
   setStatusAPI,
-} from "../api/rider-api"
+} from "../api/rider-api";
 import {
   car,
   carousel1,
@@ -24,185 +24,198 @@ import {
   person,
   redcircle,
   yellowcircle,
-} from "../assets"
-import Layout from "../components/layout/Layout"
-import AuthContext from "../context/auth-context"
-import { order_type } from "../context/context-type"
+} from "../assets";
+import Layout from "../components/layout/Layout";
+import AuthContext from "../context/auth-context";
+import { order_type } from "../context/context-type";
 
-import "./css/DriverHomePage.css"
+import "./css/DriverHomePage.css";
 
 const DriverHomePage = () => {
-  const { token, currentUser } = useContext(AuthContext)
-  const [orderList, setOrderList] = useState([order_type])
-  const [msg, setMsg] = useState("")
-  const [index, setIndex] = useState(0)
+  const { token, currentUser } = useContext(AuthContext);
+  const [orderList, setOrderList] = useState([order_type]);
+  const [msg, setMsg] = useState("");
+  const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex)
-  }
+    setIndex(selectedIndex);
+  };
 
   function handlePickUp(id) {
     postRiderOrderCreateAPI(token, id)
       .then((resp) => setMsg(resp.data.message))
-      .catch((err) => console.log(err.response))
+      .catch((err) => console.log(err.response));
   }
 
   function handleComplete(id) {
     postRiderOrderCompleteAPI(token, id)
       .then((resp) => setMsg(resp.data.message))
-      .catch((err) => console.log(err.response))
+      .catch((err) => console.log(err.response));
   }
 
   function handleStatusUpdate(statusCode) {
     setStatusAPI(token, statusCode)
       .then((resp) => setMsg(resp.data.message))
-      .catch((err) => console.log(err.response))
-    window.location.reload()
+      .catch((err) => console.log(err.response));
+    window.location.reload();
   }
 
   useEffect(() => {
     getRiderOrderAPI(token)
       .then((resp) => setOrderList(resp.data))
-      .catch((err) => console.log(err))
-  }, [token, msg])
+      .catch((err) => console.log(err));
+  }, [token, msg]);
 
   return (
     <Layout>
       <Container>
-        <Row className='my-5'>
+        <Row className="my-5">
           <Col sm={8}>
             <Carousel activeIndex={index} onSelect={handleSelect}>
               <Carousel.Item>
                 <img
-                  className='d-block w-100 carousel-member rounded'
+                  className="d-block w-100 carousel-member rounded"
                   src={carousel1}
-                  alt='First slide'
+                  alt="First slide"
                 />
                 <Carousel.Caption>
-                  <h3>Daily Meals</h3>
+                  <h3>What is Healthy Diet</h3>
                   <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                    A healthy diet is an essential aspect of a well-balanced
+                    lifestyle. It should include a variety of fruits,
+                    vegetables, whole grains, lean protein sources, and healthy
+                    fats. A healthy diet is not about strict limitations, but
+                    rather about feeling good, having more energy, and keeping
+                    your body healthy.
                   </p>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className='d-block w-100 carousel-member rounded'
+                  className="d-block w-100 carousel-member rounded"
                   src={carousel2}
-                  alt='Second slide'
+                  alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                  <h3>Daily Meals</h3>
+                  <h3>Benefit of Healthy Diet</h3>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Eating a healthy diet has numerous benefits for both
+                    physical and mental health. Eating a diet rich in nutrients
+                    also supports strong bones, healthy skin, and good eye
+                    health. Additionally, a healthy diet can improve mental
+                    clarity, increase energy levels, and boost mood. Moreover,
+                    it can aid in digestion, reduce inflammation, and support a
+                    strong immune system.
                   </p>
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
                 <img
-                  className='d-block w-100 carousel-member rounded'
+                  className="d-block w-100 carousel-member rounded"
                   src={carousel3}
-                  alt='Third slide'
+                  alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                  <h3>Daily Meals</h3>
+                  <h3>Our Meal Package</h3>
                   <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
+                    Our meal package include a balanced selection of nutritious
+                    options, including whole grains, lean proteins, and plenty
+                    of fresh fruits and vegetables. The menu also offer healthy
+                    fats and limit processed foods, added sugars, and unhealthy
+                    fats. We follow a strict regulation to make well-balanced,
+                    portion-controlled, and meet specific dietary requirements,
+                    such as low-sodium or gluten-free options.
                   </p>
                 </Carousel.Caption>
               </Carousel.Item>
             </Carousel>
           </Col>
           <Col sm={4}>
-            <div className='card'>
-              <div className='text-center'>
-                <h3 className='text-white py-3'>Profile Card</h3>
+            <div className="card">
+              <div className="text-center">
+                <h3 className="text-white py-3">Profile Card</h3>
               </div>
-              <Row className='mb-3'>
+              <Row className="mb-3">
                 <Col>
                   <img
                     src={currentUser.imageUrl}
-                    alt='profile pic'
-                    className='profile-driver'
+                    alt="profile pic"
+                    className="profile-driver"
                   />
                 </Col>
-                <Col className='text-white'>
+                <Col className="text-white">
                   <span>Name : </span>
                   <span>{currentUser.name}</span>
-                  <div className='d-flex'>
-                    <img src={car} alt='' className='icon-car' />
+                  <div className="d-flex">
+                    <img src={car} alt="" className="icon-car" />
                     <span>Driver</span>
                   </div>
                   <span>Total Delivery : </span>
                   <span>100</span>
                 </Col>
               </Row>
-              <div className='dropdown'>
-                <button className='dropbtn'>Status {currentUser.status}</button>
-                <div className='dropdown-content'>
-                  <div className='status text-white d-flex justify-content-center w-50 m-auto mb-2 mt-2'>
+              <div className="dropdown">
+                <button className="dropbtn">
+                  Status: {currentUser.status}
+                </button>
+                <div className="dropdown-content">
+                  <div className="status text-white d-flex justify-content-center w-50 m-auto mb-2 mt-2">
                     <Button
-                      className='w-100 text-left'
+                      className="w-100 text-left"
                       onClick={() => {
-                        handleStatusUpdate(1)
+                        handleStatusUpdate(1);
                       }}
                     >
-                      <img src={greencircle} alt='' className='status-icon' />
-                      <span className='fw-bold ms-3'>Available</span>
+                      <img src={greencircle} alt="" className="status-icon" />
+                      <span className="fw-bold ms-3">Available</span>
                     </Button>
                   </div>
-                  <div className='status text-white d-flex justify-content-center w-50 m-auto mb-2'>
+                  <div className="status text-white d-flex justify-content-center w-50 m-auto mb-2">
                     <Button
-                      className='w-100 text-left'
+                      className="w-100 text-left"
                       onClick={() => {
-                        handleStatusUpdate(2)
+                        handleStatusUpdate(2);
                       }}
                     >
-                      <img src={yellowcircle} alt='' className='status-icon' />
-                      <span className='fw-bold ms-3'>Busy</span>
+                      <img src={yellowcircle} alt="" className="status-icon" />
+                      <span className="fw-bold ms-3">Busy</span>
                     </Button>
                   </div>
-                  <div className='status text-white d-flex justify-content-center w-50 m-auto mb-2'>
+                  <div className="status text-white d-flex justify-content-center w-50 m-auto mb-2">
                     <Button
-                      className='w-100 text-left'
+                      className="w-100 text-left"
                       onClick={() => {
-                        handleStatusUpdate(3)
+                        handleStatusUpdate(3);
                       }}
                     >
-                      <img src={redcircle} alt='' className='status-icon' />
-                      <span className='fw-bold ms-3'>Not Available</span>
+                      <img src={redcircle} alt="" className="status-icon" />
+                      <span className="fw-bold ms-3">Not Available</span>
                     </Button>
                   </div>
                 </div>
-              </div>
-              <div className='status text-white d-flex justify-content-center my-3'>
-                {/* <img src={greencircle} alt='' className='status-icon' />
-                <span className='fw-bold ms-3'>Available</span> */}
-                {currentUser.status}
               </div>
             </div>
           </Col>
         </Row>
 
-        <div className='card mb-5'>
-          <h3 className='text-white fw-bold text-center py-3'>
+        <div className="card mb-5">
+          <h3 className="text-white fw-bold text-center py-3">
             Delivery Schedule
           </h3>
           {msg && (
             <span
               onClick={() => {
-                setMsg("")
+                setMsg("");
               }}
             >
               {msg}
             </span>
           )}
-          <div className='container'>
-            <Table striped className='text-white text-center driver mb-3'>
-              <thead className='driver-table'>
+          <div className="container">
+            <Table striped className="text-white text-center driver mb-3">
+              <thead className="driver-table">
                 <tr>
                   <th>No</th>
                   <th>Pick up</th>
@@ -211,32 +224,32 @@ const DriverHomePage = () => {
                   <th>Action</th>
                 </tr>
               </thead>
-              <tbody className='text-white'>
+              <tbody className="text-white">
                 {orderList.map((order, index) => (
                   <tr key={index}>
-                    <td className='text-white'>{index + 1}</td>
-                    <td className='text-white'>{order.preparedBy.address}</td>
-                    <td className='text-white'>{order.orderBy.address}</td>
-                    <td className='text-white'>
-                      <div className='status text-white d-flex justify-content-center'>
-                        <img src={greencircle} alt='' className='status-icon' />
-                        <span className='ms-3'>{order.orderStatus}</span>
+                    <td className="text-white">{index + 1}</td>
+                    <td className="text-white">{order.preparedBy.address}</td>
+                    <td className="text-white">{order.orderBy.address}</td>
+                    <td className="text-white">
+                      <div className="status text-white d-flex justify-content-center">
+                        <img src={greencircle} alt="" className="status-icon" />
+                        <span className="ms-3">{order.orderStatus}</span>
                       </div>
                     </td>
-                    <td className='text-white'>
+                    <td className="text-white">
                       {order.orderStatus === "READY_TO_DELIVER" ? (
                         <Button
                           onClick={() => {
-                            handlePickUp(order.id)
+                            handlePickUp(order.id);
                           }}
                         >
                           pickup
                         </Button>
                       ) : (
                         <Button
-                          variant='success'
+                          variant="success"
                           onClick={() => {
-                            handleComplete(order.id)
+                            handleComplete(order.id);
                           }}
                         >
                           complete
@@ -306,7 +319,7 @@ const DriverHomePage = () => {
         </div>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default DriverHomePage
+export default DriverHomePage;
