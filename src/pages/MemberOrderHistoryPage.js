45da6749ea4e2c9090dfd98599a2c6efd7166892
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-02-11 14:22:31 +0700

    fix text color member order history

--- src/pages/MemberOrderHistoryPage.js
+++ src/pages/MemberOrderHistoryPage.js
@@ -1,43 +1,43 @@
-import { Button, Container, Form, Table } from "react-bootstrap"
-import { useContext, useState, useEffect } from "react"
+import { Button, Container, Form, Table } from "react-bootstrap";
+import { useContext, useState, useEffect } from "react";
 import {
   getMemberOrderAllAPI,
   postMemberOrderCompleteAPI,
-} from "../api/member-api"
-import Layout from "../components/layout/Layout"
-import AuthContext from "../context/auth-context"
-import { order_type } from "../context/context-type"
+} from "../api/member-api";
+import Layout from "../components/layout/Layout";
+import AuthContext from "../context/auth-context";
+import { order_type } from "../context/context-type";
 
-import "./css/AdminDonationHistoryPage.css"
+import "./css/AdminDonationHistoryPage.css";
 
 const MemberOrderHistoryPage = () => {
-  const { token } = useContext(AuthContext)
-  const [order, setOrder] = useState([order_type])
-  const [msg, setMsg] = useState("")
+  const { token } = useContext(AuthContext);
+  const [order, setOrder] = useState([order_type]);
+  const [msg, setMsg] = useState("");
 
   function handleComplate(id) {
     postMemberOrderCompleteAPI(token, id)
       .then((resp) => setMsg(resp.data.message))
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
     getMemberOrderAllAPI(token)
       .then((resp) => setOrder(resp.data.sort((a, b) => a.id - b.id)))
-      .catch((err) => console.log(err))
+      .catch((err) => console.log(err));
 
-    return () => {}
-  }, [token])
+    return () => {};
+  }, [token]);
 
   return (
     <Layout>
       <Container>
-        <h1 className='text-center py-5 fw-bold'>Order History</h1>
+        <h1 className="text-center py-5 fw-bold">Order History</h1>
 
-        <div className='card-history'>
+        <div className="card-history">
           {msg}
-          <Table striped className='text-white text-center history-table mb-5'>
-            <thead className='history-table text-dark'>
+          <Table striped className="text-white text-center history-table mb-5">
+            <thead className="history-table text-dark">
               <tr>
                 <th>No</th>
                 <th>Meal Package</th>
@@ -48,13 +48,18 @@ const MemberOrderHistoryPage = () => {
                 <th>Status</th>
               </tr>
             </thead>
-            <tbody className='text-white'>
+            <tbody className="text-dark">
               {order.map((x, i) => (
                 <tr key={x.id}>
                   <td>{i + 1}</td>
                   <td>{x.mealPackage.packageName}</td>
                   <td>{x.preparedBy?.name}</td>
-                  <td>{new Date(x.orderOn).toLocaleString('en-GB', { timeZone: 'Asia/Singapore',hour12:true })}</td>
+                  <td>
+                    {new Date(x.orderOn).toLocaleString("en-GB", {
+                      timeZone: "Asia/Singapore",
+                      hour12: true,
+                    })}
+                  </td>
                   <td>{x.orderBy.address}</td>
                   <td>{x.deliveredBy?.name}</td>
                   <td>{x.orderStatus}</td>
@@ -65,7 +70,7 @@ const MemberOrderHistoryPage = () => {
         </div>
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default MemberOrderHistoryPage
+export default MemberOrderHistoryPage;
